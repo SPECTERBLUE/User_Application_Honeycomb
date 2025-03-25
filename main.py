ea81673892bb784672545507e8c3a5598a8c35e4
AKHIL <akhilesh@meridiandatalabs.com>
2025-03-25 16:02:21 +0530

    Api's for downlink-1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 import logging
 import config
 import grpc
+import uvicorn
 from scheduler import start_scheduler
 from event_fetcher_parse import initialize_key_rotation, start_mqtt_client
 
@@ -13,8 +14,16 @@ logger = logging.getLogger(__name__)
 channel = grpc.insecure_channel(config.CHIRPSTACK_HOST)  # Set up your gRPC channel here
 auth_token = config.AUTH_METADATA  # Set your authentication token here
 
+def run_api():
+    """Function to run FastAPI server in a separate thread."""
+    uvicorn.run("api_downlink:app", host="0.0.0.0", port=4567, reload=False)
+
 if __name__ == "__main__":
     try:
+        logger.info("Starting API server...")
+        api_thread = threading.Thread(target=run_api, daemon=True)
+        api_thread.start()
+
         logger.info("Starting device scheduler...")
         scheduler_thread = threading.Thread(target=start_scheduler, daemon=True)
         scheduler_thread.start()
